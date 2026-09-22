98eb17ff0687184d1b5b86a99513e6eb9a457ff7
Vladislav Shcherbatov <vladislav.a.shcherbatov@gmail.com>
2026-09-22 22:01:37 +0200

    Added 04-rwmutex

---
+++ 04-Goroutines/rwmutex/rwmutex.go
@@ -0,0 +1,77 @@
+//go:build !solution
+
+package rwmutex
+
+// A RWMutex is a reader/writer mutual exclusion lock.
+// The lock can be held by an arbitrary number of readers or a single writer.
+// The zero value for a RWMutex is an unlocked mutex.
+//
+// If a goroutine holds a RWMutex for reading and another goroutine might
+// call Lock, no goroutine should expect to be able to acquire a read lock
+// until the initial read lock is released. In particular, this prohibits
+// recursive read locking. This is to ensure that the lock eventually becomes
+// available; a blocked Lock call excludes new readers from acquiring the
+// lock.
+type RWMutex struct {
+	accessLock  chan struct{}
+	counterLock chan struct{}
+	cntReaders  int
+}
+
+// New creates *RWMutex.
+func New() *RWMutex {
+	rw := &RWMutex{
+		accessLock:  make(chan struct{}, 1),
+		counterLock: make(chan struct{}, 1),
+	}
+
+	return rw
+}
+
+// RLock locks rw for reading.
+//
+// It should not be used for recursive read locking; a blocked Lock
+// call excludes new readers from acquiring the lock. See the
+// documentation on the RWMutex type.
+func (rw *RWMutex) RLock() {
+	rw.counterLock <- struct{}{}
+
+	rw.cntReaders++
+	if rw.cntReaders == 1 {
+		rw.accessLock <- struct{}{}
+	}
+
+	<-rw.counterLock
+}
+
+// RUnlock undoes a single RLock call;
+// it does not affect other simultaneous readers.
+// It is a run-time error if rw is not locked for reading
+// on entry to RUnlock.
+func (rw *RWMutex) RUnlock() {
+	rw.counterLock <- struct{}{}
+
+	rw.cntReaders--
+	if rw.cntReaders == 0 {
+		<-rw.accessLock
+	}
+
+	<-rw.counterLock
+}
+
+// Lock locks rw for writing.
+// If the lock is already locked for reading or writing,
+// Lock blocks until the lock is available.
+func (rw *RWMutex) Lock() {
+	rw.accessLock <- struct{}{}
+}
+
+// Unlock unlocks rw for writing. It is a run-time error if rw is
+// not locked for writing on entry to Unlock.
+//
+// As with Mutexes, a locked RWMutex is not associated with a particular
+// goroutine. One goroutine may RLock (Lock) a RWMutex and then
+// arrange for another goroutine to RUnlock (Unlock) it.
+func (rw *RWMutex) Unlock() {
+	<-rw.accessLock
+}
